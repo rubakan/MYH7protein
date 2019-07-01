36937aa042dd11e0fb9a57c9d25d476350f7cd0e
rubakan <rubakan.thurupan@nmbu.no>
2019-07-01 15:12:20 +0100

    Updated version

--- myh7_analysis/ExtractExcelMutations.py
+++ myh7_analysis/ExtractExcelMutations.py
@@ -25,15 +25,45 @@
 #In this script I have extracted from the Excel document that we received 
 #from cardiologist Dr.Lopes Luis from St Bartholomew's Hospital. 
 #The goal is to use this script on Dr. Andrew Martin's bioinformatic tool to analyze.(?)
-#*********************************************************************************************************
-
+#
+#********************************************************************************************************************
+#
+#   Description:
+#   ============
+#
+#********************************************************************************************************************
+#
+#   Usage:
+#   ======
+#   Extract a Excel file to Python Script.
+#
+#********************************************************************************************************************
+#
+#   Revision History:
+#   =================
+#   V1.0   25.06.19  Original
+#   V1.1   27.06.19  Output format   Changed uniprot accession to be at 
+#                                    the start of each line rather than a counter 
+#                                        
+#                    file            Changed filename from "MYH7protein.py to
+#                                    ExtractExcelMutations.py.
+#                    
+#   V1.2   28.06.19  Import          added the add_argument() method, which is what we use to specify which 
+#                    Argparse        command-line options the program is willing to accept. 
+#                                    
+#                    
+#
+#
+#********************************************************************************************************************
 import pandas as pd
 import re
 
 
 def extract_mutants(df):
     """
-    Here I am trying to extract mutant from the excel document
+    Here I am trying to extract mutant from the excel document.
+    We import the pandas module, including ExcelFile. 
+    The df reads the data into a Pandas Data Frame.
     """
     
     native_residue=list() #List of native residue
@@ -60,12 +90,16 @@ def main(file, accession_code):
     native_residue, residue_number, mutant_residue = extract_mutants(df)
 
 if __name__ == "__main__":
+    """
+    
+    """
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="file to analyze", type=str)
+    parser.add_argument("column_name", help="column name", type=str)
     parser.add_argument("accession_code", help="accession code", type=str)
     args = parser.parse_args()
-    main(args.file, args.accession_code)
+    main(args.file,args.column_name,args.accession_code)
     
 
    
